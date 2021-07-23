e817de4c5d7d1461a40f42d60005d0bdf3583894
gayathri-rajamanickam4 <gayathri4984@gmail.com>
2021-07-23 21:03:45 +0530

    prod config error corrected

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -13,7 +13,7 @@ const prodConfig = {
       name: 'marketing',
       filename: 'remoteEntry.js',
       exposes: {
-        './MarketingApp': './src/boostrap',
+        './MarketingApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
